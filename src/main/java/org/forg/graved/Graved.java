67b8d294951dbd05fec769599d98ab7d7b49ef19
frogman <74905689+TheRealFrogman@users.noreply.github.com>
2024-07-14 17:14:44 +0500

    fixed persistence between sessions

--- src/main/java/org/forg/graved/Graved.java
+++ src/main/java/org/forg/graved/Graved.java
@@ -4,9 +4,7 @@ import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Chest;
-import org.bukkit.entity.Display;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.TextDisplay;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -15,6 +13,7 @@ import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -22,34 +21,41 @@ import org.bukkit.plugin.java.JavaPlugin;
 import javax.annotation.Nullable;
 import javax.annotation.Nonnull;
 
-import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 class GravedChest {
-    private static final HashMap<GravedChest, TextDisplay> chest_to_text = new HashMap<GravedChest, TextDisplay>();
-    private static final NamespacedKey GRAVED_META_KEY
+    private static final NamespacedKey GRAVED_CHEST_META_KEY
             = new NamespacedKey(Graved.getInstance(), "GRAVED_CHEST");
+    private static final NamespacedKey CHEST_DISPLAY_UUID
+            = new NamespacedKey(Graved.getInstance(), "CHEST_DISPLAY");
 
     public final Chest c;
     public final Player p;
-    public final String bottomtext;
-    private GravedChest(Chest c, Player p, String bottomtext) {
+    private GravedChest(Chest c, Player p) {
         this.c = c;
         this.p = p;
-        this.bottomtext = bottomtext;
     }
     public static GravedChest createAndRegister(Chest c, Player p, List<ItemStack> contents, String toptext,String bottomtext){
-        GravedChest gc = new GravedChest(c, p, bottomtext);
-        gc.c.getPersistentDataContainer().set(GRAVED_META_KEY, PersistentDataType.BOOLEAN,true);
-        gc.c.update();
+        GravedChest gc = new GravedChest(c, p);
 
-        Inventory chest_inventory = c.getBlockInventory();
-        for(ItemStack item : contents){
+        PersistentDataContainer chestMetaContainer = gc.c.getPersistentDataContainer();
+
+        TextDisplay display = makeGravedChestDisplay(c,p,toptext,bottomtext);
+
+        chestMetaContainer.set(CHEST_DISPLAY_UUID, PersistentDataType.STRING, display.getUniqueId().toString());
+        chestMetaContainer.set(GRAVED_CHEST_META_KEY, PersistentDataType.BOOLEAN,true);
+
+        gc.c.update(); // required fsr
+
+        Inventory chest_inventory = gc.c.getBlockInventory();
+        for (ItemStack item : contents) {
             if(item != null) {
                 chest_inventory.addItem(item);
             }
         }
-        chest_to_text.put(gc, makeGravedChestDisplay(c,p,toptext,bottomtext));
+
+
         return gc;
     }
     @Nonnull
@@ -71,23 +77,30 @@ class GravedChest {
         );
         return text_display;
     }
-    @Nullable
-    public static GravedChest GetIfGraved(Chest c) {
 
-        if (c.getPersistentDataContainer().has(GRAVED_META_KEY, PersistentDataType.BOOLEAN)) {
-            // Find the GravedChest instance associated with this Chest
-            for (GravedChest gc : chest_to_text.keySet()) {
-                if (gc.c.getBlock().getState().equals(c.getBlock().getState())) {
-                    return gc;
-                }
-            }
+    @Nullable
+    public static GravedChest GetIfGraved(Chest c, Player p) {
+        PersistentDataContainer chestPersistentMeta = c.getPersistentDataContainer();
+        if (
+            chestPersistentMeta.has(GRAVED_CHEST_META_KEY)
+            && chestPersistentMeta.has(CHEST_DISPLAY_UUID)
+        ) {
+            return new GravedChest(c,p);
         }
         return null;
     }
     void remove() {
+        PersistentDataContainer chestPersistentMeta = this.c.getPersistentDataContainer();
         this.c.getBlock().setType(Material.AIR);
-        chest_to_text.get(this).remove();
-        chest_to_text.remove(this);
+
+        if (!chestPersistentMeta.has(GRAVED_CHEST_META_KEY)) return;
+        String maybeUUID = chestPersistentMeta.get(CHEST_DISPLAY_UUID, PersistentDataType.STRING);
+        if(maybeUUID == null) return;
+        UUID displayUUID = UUID.fromString(maybeUUID);
+
+        Entity display = Graved.getInstance().getServer().getEntity(displayUUID);
+        if(display == null) return;
+        display.remove();
     }
 
     boolean isEmpty(){
@@ -123,7 +136,7 @@ public class Graved extends JavaPlugin implements Listener {
 
         Location location = p.getLocation();
 
-        // здесь наверное нужно будет в сфере 3 на 3 искать рандомный блок куда можно поставить сундукы
+        // defining the block to change
         Block b = location.getBlock();
 
         b.setType(Material.CHEST);
@@ -138,10 +151,15 @@ public class Graved extends JavaPlugin implements Listener {
     void onChestClosed(InventoryCloseEvent e) {
         Inventory i = e.getInventory();
         InventoryHolder ih = i.getHolder();
+
+        // e.getPlayer returns HumanEntity not player ???
+        HumanEntity he = e.getPlayer();
+        Player p = getServer().getPlayer(he.getName());
+        //
+
         if(!(ih instanceof  Chest)) return;
         Chest c = (Chest)ih;
-
-        GravedChest gc = GravedChest.GetIfGraved(c);
+        GravedChest gc = GravedChest.GetIfGraved(c, p);
         if(gc == null) return;
 
         if (gc.isEmpty()) gc.remove();
@@ -154,7 +172,7 @@ public class Graved extends JavaPlugin implements Listener {
         if(!(bs instanceof  Chest)) return;
         Chest c = (Chest)bs;
 
-        GravedChest gc = GravedChest.GetIfGraved(c);
+        GravedChest gc = GravedChest.GetIfGraved(c, e.getPlayer());
         if(gc == null) return;
 
         gc.remove();
